97729259eab3295e71417288c2dfea971b8d12bf
Pushpa Saud <157209278+pushpasaud@users.noreply.github.com>
2024-10-10 14:38:02 +0545

    Sum of two numbers

---
+++ Sum.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,A;
+	printf("Enter any two numbers");
+	scanf("%d %d %d",&a,&b,&A);
+	A=(a+b);
+	printf("The sum of two numbers is %d",&A);
+	return 0;
+}
